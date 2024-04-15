ab1ab7a0754c6915e2d91b53f636453fbc149c08
Gregory Harris <63122765+gregoryharris869@users.noreply.github.com>
2024-04-15 16:44:12 -0500

    Update Home.tsx

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -4,25 +4,28 @@ import { motion } from "framer-motion";
 
 type Props = { setSelectedPage: (value: SelectedPage) => void };
 
+const textVariant = "text-2xl font-semibold";
+
 const Home = ({ setSelectedPage }: Props) => {
   return (
     <section id="home" className="py-10 md:py-32 ">
       <motion.div
-        className="container max-w-screen-xl mx-auto px-4"
+        className="container max-w-screen-xl px-4 mx-auto"
         onViewportEnter={() => setSelectedPage(SelectedPage.Home)}
       >
         <div className="text-center">
-          <h6 className="mt-16 font-medium text-gray-600 text-lg md:text-2xl uppercase mb-8">
+          <h6 className="mt-16 mb-8 text-lg font-medium text-gray-600 uppercase md:text-2xl">
             Gregory Harris
           </h6>
 
-          <h1 className="font-normal text-gray-900 text-4xl md:text-7xl leading-none mb-8">
+          <h1 className="mb-8 text-4xl font-normal leading-none text-gray-900 md:text-7xl">
             Front-end Developer
           </h1>
 
-          <p className="font-normal text-gray-600 text-md md:text-xl mb-16">
-            As a passionate front-end developer, with focus on React and
-            Next.js.
+          <p className="mb-16 font-normal text-gray-600 text-md md:text-xl">
+            A passionate front-end developer, with focus on{" "}
+            <span className={textVariant}>React</span> and{" "}
+            <span className={textVariant}>Next.js</span>.
           </p>
 
           <ActionButton setSelectedPage={setSelectedPage}>Contact</ActionButton>
