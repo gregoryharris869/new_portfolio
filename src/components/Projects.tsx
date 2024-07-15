779d170dad274e50c383a738d39ecf62ade18c24
Gregory Harris <63122765+gregoryharris869@users.noreply.github.com>
2024-07-15 16:15:46 -0500

    updated

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Project from "./Project";
+
 import { projectsData } from "../lib/data";
 import { SelectedPage } from "../shared/types";
 import { motion } from "framer-motion";
